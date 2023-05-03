20bf9de7fe1ae055d9e9f20bfc8980a63c093889
Svante Boberg <svante.boberg@gmail.com>
2023-05-03 13:40:17 +0200

    Set styling with css or svg props

--- scripts/generate-react.js
+++ scripts/generate-react.js
@@ -3,10 +3,8 @@ const generate = require('./generate');
 generate('react', component => `import React from 'react';
 
 const ${component.name} = ({ color = 'currentColor', size = 24, children, ...props }) => {
-  const className = 'remixicon-icon ' + (props.className || '');
-
   return (
-    <svg {...props} className={className} width={size} height={size} fill={color} viewBox="0 0 24 24">
+    <svg {...props} viewBox="0 0 24 24">
       <path d="${component.svgPath}" />
     </svg>
   );
@@ -21,11 +19,7 @@ export default ${component.name};
 
 type AllSVGProps = SVGProps<SVGSVGElement>
 
-type ReservedProps = 'color' | 'size' | 'width' | 'height' | 'fill' | 'viewBox'
-
-export interface RemixiconReactIconProps extends Pick<AllSVGProps, Exclude<keyof AllSVGProps, ReservedProps>> {
-  color?: string;
-  size?: number | string;
+export interface RemixiconReactIconProps extends Pick<AllSVGProps, Exclude<keyof AllSVGProps, 'viewBox'>> {
   // should not have any children
   children?: never;
 }
