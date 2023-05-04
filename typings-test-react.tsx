01c533f31bdc86be29635a6d05e91eb7adda553f
Svante Boberg <svante.boberg@gmail.com>
2023-05-04 12:00:01 +0200

    Update typings test

--- typings-test-react.tsx
+++ typings-test-react.tsx
@@ -1,23 +1,24 @@
-import AccessPointIcon from './publish-react/AccessPointIcon';
+import React from 'react'
+import SvgAccountBoxFill from './publish-react/SvgAccountBoxFill';
+import { SvgAccountBoxLine } from './publish-react'
 
 // this will throw a type error
 const noRender = () => (
   <>
-    <AccessPointIcon>Things that are not shown</AccessPointIcon>
-    <AccessPointIcon color={0} />
-    <AccessPointIcon size />
-    <AccessPointIcon className={5} />
+    <SvgAccountBoxFill>Things that are not shown</AccessPointIcon>
+    <SvgAccountBoxFill color={0} />
+    <SvgAccountBoxLine size />
+    <SvgAccountBoxLine className={5} />
   </>
 );
 
 // this should be the one to use
 const render = () => (
   <>
-    <AccessPointIcon></AccessPointIcon>
-    <AccessPointIcon size={16} />
-    <AccessPointIcon color="#fff" size="16" className="some-class" />
-    <AccessPointIcon
-      size={16} style={{display: 'block', margin: 'auto'}}
+    <SvgAccountBoxFill></SvgAccountBoxFill>
+    <SvgAccountBoxFill fill="#fff" className="some-class" />
+    <SvgAccountBoxLine
+      style={{display: 'block', margin: 'auto'}}
       onClick={() => {}} />
   </>
 );
